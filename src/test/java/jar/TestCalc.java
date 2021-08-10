c38e6a8375e87dd306355270669f2abe521fa455
ssszheng <302871958@qq.com>
2021-08-10 13:03:25 +1200

    tutorial4 -task1

---
+++ src/test/java/jar/TestCalc.java
@@ -0,0 +1,19 @@
+package jar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for Calc class.
+ * 
+ */
+class CalcTest{
+	
+	@Test
+	public void addTest(){
+		
+		Calc myCalc = new Calc();
+		int sum = myCalc.add(1, 4);
+		Assertions.assertEquals(sum, 5);
+	}
+}
